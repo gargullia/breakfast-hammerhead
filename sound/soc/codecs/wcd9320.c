gargullia/breakfast-hammerhead

commit 83158eb4eeab7dff9f773d1d5e53322ed858189b

ASoC: wcd9320: Use sound control data to expose sound gains
Register a new sound control data to take advantage of the sysfs-based
sound gain controller and export codec's audio lines' values to SysFS.

Change-Id: I697f1ed35324d9b8d28eddd9a346aeebafd2e04e
Signed-off-by: Alex Saiko <solcmdr@gmail.com>

sound/soc/codecs/wcd9320.c
@@ -23,6 +23,7 @@
 #include <linux/mfd/wcd9xxx/core.h>
 #include <linux/mfd/wcd9xxx/wcd9xxx_registers.h>
 #include <linux/mfd/wcd9xxx/wcd9320_registers.h>
+#include <linux/mfd/wcd9xxx/wcd9xxx-snd-ctrl.h>
 #include <linux/mfd/wcd9xxx/pdata.h>
 #include <linux/regulator/consumer.h>
 #include <sound/pcm.h>
@@ -466,6 +467,7 @@ static const u32 vport_i2s_check_table[NUM_CODEC_DAIS] = {
 
 struct taiko_priv {
 	struct snd_soc_codec *codec;
+	struct snd_ctrl_data *ctrl_data;
 	u32 adc_count;
 	u32 rx_bias_count;
 	s32 dmic_1_2_clk_cnt;
@@ -6572,6 +6574,24 @@ static int taiko_codec_probe(struct snd_soc_codec *codec)
 		goto err_pdata;
 	}
 
+	taiko->ctrl_data = kzalloc(sizeof(*taiko->ctrl_data), GFP_KERNEL);
+	if (IS_ERR_OR_NULL(taiko->ctrl_data)) {
+		dev_err(codec->dev, "Failed to allocate control data\n");
+		goto fail_ctrl;
+	}
+
+	taiko->ctrl_data->codec = codec;
+	taiko->ctrl_data->name = codec->name;
+	taiko->ctrl_data->read = &taiko_read;
+	taiko->ctrl_data->write = &taiko_write;
+
+	ret = snd_ctrl_register(taiko->ctrl_data);
+	if (IS_ERR_VALUE(ret)) {
+		pr_err("%s: Unable to register control data\n", __func__);
+		kfree(taiko->ctrl_data);
+	}
+
+fail_ctrl:
 	taiko->spkdrv_reg = taiko_codec_find_regulator(codec,
 						       WCD9XXX_VDD_SPKDRV_NAME);
 
@@ -6665,6 +6685,7 @@ err_irq:
 err_pdata:
 	kfree(ptr);
 err_nomem_slimch:
+	kfree(taiko->ctrl_data);
 	kfree(taiko);
 err_init:
 	return ret;
@@ -6690,6 +6711,9 @@ static int taiko_codec_remove(struct snd_soc_codec *codec)
 
 	taiko->spkdrv_reg = NULL;
 
+	snd_ctrl_unregister(taiko->ctrl_data);
+
+	kfree(taiko->ctrl_data);
 	kfree(taiko);
 	return 0;
 }

commit 3489e571bce89d8fe02e483f58bd8dd090824007

ASoC: wcd9320: Workaround a conflict with Android's AudioHAL
This was explained in commit caa2a40c65af.

Unfortunately, there is no other option, because Android's AudioHAL
actively uses IOCTL (just like any other normal client) to setup sound
lines.  If we want to completely switch controls from IOCTL to simple
sysfs, we need to entirely filter out writing to target lines from it.

Fixes: 83158eb4eeab ("ASoC: wcd9320: Use sound control data to expose sound gains")
Change-Id: I708022085bc56483e44494bb5f222d9f4a4ba11f
Signed-off-by: Alex Saiko <solcmdr@gmail.com>

sound/soc/codecs/wcd9320.c
@@ -4176,6 +4176,32 @@ static unsigned int taiko_read(struct snd_soc_codec *codec,
 	return val;
 }
 
+static int taiko_filter(struct snd_soc_codec *codec, unsigned int reg)
+{
+	struct taiko_priv *taiko = snd_soc_codec_get_drvdata(codec);
+	struct snd_ctrl_data *snd_data = taiko->ctrl_data;
+
+	if (IS_ERR_OR_NULL(snd_data) || !snd_ctrl_data_handled(snd_data) ||
+	  !(snd_data->flags & SND_CTRL_BYPASS_IOCTL))
+		return 0;
+
+	/* Sort in use-frequency order */
+	if (reg == snd_data->lines.speaker_l_line)
+		return -EINVAL;
+	else if (reg == snd_data->lines.speaker_r_line)
+		return -EINVAL;
+	else if (reg == snd_data->lines.headphone_l_line)
+		return -EINVAL;
+	else if (reg == snd_data->lines.headphone_r_line)
+		return -EINVAL;
+	else if (reg == snd_data->lines.cam_mic_line)
+		return -EINVAL;
+	else if (reg == snd_data->lines.mic_line)
+		return -EINVAL;
+
+	return 0;
+}
+
 static int taiko_startup(struct snd_pcm_substream *substream,
 		struct snd_soc_dai *dai)
 {
@@ -6584,6 +6610,7 @@ static int taiko_codec_probe(struct snd_soc_codec *codec)
 	taiko->ctrl_data->name = codec->name;
 	taiko->ctrl_data->read = &taiko_read;
 	taiko->ctrl_data->write = &taiko_write;
+	taiko->ctrl_data->flags = SND_CTRL_BYPASS_IOCTL;
 
 	ret = snd_ctrl_register(taiko->ctrl_data);
 	if (IS_ERR_VALUE(ret)) {
@@ -6723,6 +6750,7 @@ static struct snd_soc_codec_driver soc_codec_dev_taiko = {
 
 	.read = taiko_read,
 	.write = taiko_write,
+	.filter = taiko_filter,
 
 	.readable_register = taiko_readable,
 	.volatile_register = taiko_volatile,